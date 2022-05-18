3877a4a08782a64f055f4e5082c85a218bed7796
ulazy1 <83034407+ulazy1@users.noreply.github.com>
2022-05-18 17:17:00 -0400

    customize webkit scrollbar

--- src/inject-css.js
+++ src/inject-css.js
@@ -31,6 +31,20 @@ const InjectCSS = () => {
     div#container, div#content {
         width: auto;
     }
+
+    ::-webkit-scrollbar {
+      width: 6px;
+    }
+    
+    ::-webkit-scrollbar-track {
+        -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3); 
+        border-radius: 10px;
+    }
+    
+    ::-webkit-scrollbar-thumb {
+        border-radius: 10px;
+        -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.5); 
+    }
     </style>
     `;
 };
