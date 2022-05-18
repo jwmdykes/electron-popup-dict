e97bcf42665c2615b1de001a9bafc703d0aa8cf5
ulazy1 <83034407+ulazy1@users.noreply.github.com>
2022-05-18 12:02:30 -0400

    reformat main.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-const settings = require('./settings');
-
+/////////////////////////////////////////////
+// For debugging, print out errors to console
 process.on('uncaughtException', (error) => {
   console.log(error);
   app.quit();
   process.exit();
 });
+// For debugging, print out errors to console
+/////////////////////////////////////////////
 
+const settings = require('./settings');
 const { registerHotkeys } = require('./hotkeys.js');
 
 const createWindow = () => {
@@ -32,6 +35,9 @@ const createWindow = () => {
   return win;
 };
 
+// allow cross origin javascript so we can insert css into the iframe of the dictionary
+app.commandLine.appendSwitch('disable-site-isolation-trials');
+
 app
   .whenReady()
   .then(() => {
@@ -41,6 +47,7 @@ app
     registerHotkeys(win, app);
   });
 
+// for macos, don't close the app when the last window is closed
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
@@ -48,6 +55,3 @@ app.on('activate', () => {
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
 });
-
-// fixes cross origin error
-app.commandLine.appendSwitch('disable-site-isolation-trials');
