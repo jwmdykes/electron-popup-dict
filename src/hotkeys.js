f13b40c8b96ab4aab41e3fc0a833e72a8ac4d727
ulazy1 <83034407+ulazy1@users.noreply.github.com>
2022-05-18 11:55:24 -0400

    Move query related code to own file
    Also prettier formatting

--- src/hotkeys.js
+++ src/hotkeys.js
@@ -1,50 +1,18 @@
-const { globalShortcut} = require('electron')
-const { getMousePos, getText } = require('./python-shell')
-const { getWindowPosition, showWindow } = require("./utils")
-const { monitors, windowSize, getMonitors } = require("./settings")
-
-let PreviousText = {text: ''}
+const { globalShortcut } = require('electron');
+const { query } = require('./dict-query');
 
 const registerHotkeys = (win, app) => {
-    globalShortcut.register('esc', () => {
-        // console.log("esc")
-        app.quit()
-    })
+  globalShortcut.register('esc', () => {
+    // console.log("esc")
+    app.quit();
+  });
 
-    globalShortcut.register('CommandOrControl+D', () => {
-        // console.log("ctrl+d")
-        getText((text) => {
-            if (text.text !== '' && text.text !== PreviousText.text) {
-                win.webContents.send('change-iframe', {
-                    url: "https://ko.dict.naver.com/search.nhn?query=<<word>>&target=dic",
-                    text: text.text
-                })
-            }
-            PreviousText = text
-        })
-        getMousePos((mousePos) => {
-            // console.log(`MOUSEPOS: ${JSON.stringify(mousePos)}`)
-            getMonitors(app).then((mons) => {
-                // console.log(JSON.stringify(mons))
-                const bounds = []
-                for (const mon of mons) {
-                    mon.bounds.x1 = mon.bounds.x + mon.bounds.width
-                    mon.bounds.y1 = mon.bounds.y + mon.bounds.height
-                    bounds.push(mon.bounds)
-                }
-                return bounds
-            }).then((bounds) => {
-                const winPos = getWindowPosition(mousePos, bounds, windowSize)
-                return winPos
-            }).then((winPos) => {
-                win.setPosition(winPos.x, winPos.y)
-            }).then(() => {
-                showWindow(win, app)
-            })
-        })
-    })
-}
+  globalShortcut.register('CommandOrControl+D', () => {
+    // console.log("ctrl+d")
+    query(win, app);
+  });
+};
 
 module.exports = {
-    registerHotkeys: registerHotkeys,
-}
+  registerHotkeys: registerHotkeys,
+};
--- src/settings.js
+++ src/settings.js
@@ -13,18 +13,4 @@ module.exports = {
         height: 550,
     },
     getMonitors: getMonitors,
-    monitors: {
-        left: {
-            x0: 0,
-            x1: 1920,
-            y0: 0,
-            y1: 1080
-        },
-        right: {
-            x0: 1921,
-            x1: 3840,
-            y0: 0,
-            y1: 1080,
-        }
-    }
 }
